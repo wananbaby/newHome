0c43677693afd477438d4290c94fb7402c871358
unknown <wb.zhangyanshen@mesg.corp.netease.com>
2021-08-18 11:35:19 +0800

    Updates

---
+++ precache-manifest.ee859bf653f8243796f3cfacff528faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bb95f9de4b78a8f512e",
+    "url": "/newHome/static/css/main.cebaa4e7.chunk.css"
+  },
+  {
+    "revision": "5bb95f9de4b78a8f512e",
+    "url": "/newHome/static/js/main.5bb95f9d.chunk.js"
+  },
+  {
+    "revision": "286e97e6041e1a9d6eda",
+    "url": "/newHome/static/js/runtime~main.286e97e6.js"
+  },
+  {
+    "revision": "9d0ef1a548f181f53991",
+    "url": "/newHome/static/css/2.91ce0947.chunk.css"
+  },
+  {
+    "revision": "9d0ef1a548f181f53991",
+    "url": "/newHome/static/js/2.9d0ef1a5.chunk.js"
+  },
+  {
+    "revision": "da707f25414ecc7280ff9f2d9608c854",
+    "url": "/newHome/index.html"
+  }
+];
